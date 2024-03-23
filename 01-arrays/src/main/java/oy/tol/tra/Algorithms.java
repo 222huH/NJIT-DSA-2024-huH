2009f894995636979198310af16b01ab0c4ead18
222huH <2469801947@qq.com>
2024-03-23 21:48:56 +0800

    modified:   01-arrays/src/main/java/oy/tol/tra/Algorithms.java

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -2,13 +2,13 @@ package oy.tol.tra;
 
 public class Algorithms {
 
-    public static <T extends Comparable<T>> void sort(T[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i].compareTo(arr[j]) > 0) {
-                    T temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
             }
         }

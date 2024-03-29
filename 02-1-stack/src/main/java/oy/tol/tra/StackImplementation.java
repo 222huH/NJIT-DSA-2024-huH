ad71ea35a678e67ffc1ab789f4571e1ee97d86cd
222huH <2469801947@qq.com>
2024-03-29 12:08:51 +0800

    modified:   02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java

--- 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
+++ 02-1-stack/src/main/java/oy/tol/tra/StackImplementation.java
@@ -1,107 +1,87 @@
 package oy.tol.tra;
 
-/**
- * An implementation of the StackInterface.
- * <p>
- * Note that you need to implement construtor(s) for your concrete StackImplementation, which
- * allocates the internal Object array for the Stack:
- * - a default constructor, calling the StackImplementation(int size) with value of 10.
- * - StackImplementation(int size), which allocates an array of Object's with size.
- *  -- remember to maintain the capacity and/or currentIndex when the stack is manipulated.
- */
 public class StackImplementation<E> implements StackInterface<E> {
 
-   private Object [] itemArray;
+   private Object[] itemArray;
    private int capacity;
    private int currentIndex = -1;
    private static final int DEFAULT_STACK_SIZE = 10;
 
-   /**
-    * Allocates a stack with a default capacity.
-    * @throws StackAllocationException
-    */
    public StackImplementation() throws StackAllocationException {
-      capacity=DEFAULT_STACK_SIZE;
-
-       itemArray=new Object[DEFAULT_STACK_SIZE];
+      capacity = DEFAULT_STACK_SIZE;
+      itemArray = new Object[DEFAULT_STACK_SIZE];
    }
 
-   /**
-    * @param capacity The capacity of the stack.
-    * @throws StackAllocationException If cannot allocate room for the internal array.
-    */
    public StackImplementation(int capacity) throws StackAllocationException {
-      if(capacity<2){ 
-         throw new StackAllocationException("cannot allocate room for the internal array");
+      if (capacity < 2) {
+         throw new StackAllocationException("Cannot allocate room for the internal array");
       }
-      this.capacity=capacity;
-      itemArray=new Object[capacity];
+      this.capacity = capacity;
+      itemArray = new Object[capacity];
    }
 
    @Override
    public int capacity() {
       return this.capacity;
-      
    }
 
    @Override
    public void push(E element) throws StackAllocationException, NullPointerException {
-         if(size()==capacity()){
-         Object[] tmp=new Object[this.capacity*2+1];
-         
+      if (element == null) {
+         throw new NullPointerException("Element cannot be null");
+      }
+
+      if (size() == capacity()) {
+         Object[] tmp = new Object[this.capacity * 2 + 1];
+
          for (int i = 0; i < itemArray.length; i++) {
-            tmp[i]=itemArray[i];
+            tmp[i] = itemArray[i];
          }
-         itemArray=tmp;
-         tmp=null;
-         capacity=capacity*2+1;
+         itemArray = tmp;
+         capacity = capacity * 2 + 1;
       }
-      if(element==null){
-         throw new NullPointerException();
-      }
-      itemArray[++currentIndex]=element;      
+      itemArray[++currentIndex] = element;
    }
 
    @SuppressWarnings("unchecked")
    @Override
    public E pop() throws StackIsEmptyException {
-      if(isEmpty()){
+      if (isEmpty()) {
          throw new StackIsEmptyException("Stack is empty");
       }
-      return (E)itemArray[currentIndex--];
+      E poppedElement = (E) itemArray[currentIndex];
+      itemArray[currentIndex--] = null; // Remove the element from internal storage
+      return poppedElement;
    }
 
    @SuppressWarnings("unchecked")
    @Override
    public E peek() throws StackIsEmptyException {
-      if(isEmpty()){
+      if (isEmpty()) {
          throw new StackIsEmptyException("Stack is empty");
       }
-      return (E)itemArray[currentIndex];
+      return (E) itemArray[currentIndex];
    }
 
    @Override
    public int size() {
-      return currentIndex+1;
-      
+      return currentIndex + 1;
    }
 
    @Override
    public void clear() {
-      currentIndex=-1;
-      
+      currentIndex = -1;
    }
 
    @Override
    public boolean isEmpty() {
-      return currentIndex==-1;
-      
+      return currentIndex == -1;
    }
 
    @Override
    public String toString() {
       StringBuilder builder = new StringBuilder("[");
-      for (var index = 0; index <= currentIndex; index++) {
+      for (int index = 0; index <= currentIndex; index++) {
          builder.append(itemArray[index].toString());
          if (index < currentIndex) {
             builder.append(", ");
